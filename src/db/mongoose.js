17b881cf87505d0c5983c6239b8501f9465c7149
Abhishek Chaudhari <abhishek.chaudhari44@gmail.com>
2021-05-17 09:19:26 +0530

    added test code for user collection

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,9 +3,30 @@ const chalk = require('chalk')
 
 const errChalk = chalk.white.bgRed
 const statusChalk = chalk.greenBright.bgBlue
+const log = console.log
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
     useNewUrlParser: true, 
-    // useUnifiedTopology: true, 
+    useUnifiedTopology: true, 
     useCreateIndex: true
+})
+
+const User = mongoose.model('User', {
+    name: {
+        type: String
+    },
+    age: {
+        type: Number
+    }
+})
+
+const me = new User({
+    name: 'jax',
+    age: 36
+})
+
+me.save().then( (result) => {
+    log(statusChalk(result))
+}).catch((error) => {
+    log(errChalk(error))
 })
