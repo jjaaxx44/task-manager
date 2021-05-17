ea8e3ec8c1647dcba767cb7c9a9c9ff27303300c
Abhishek Chaudhari <abhishek.chaudhari44@gmail.com>
2021-05-17 10:54:50 +0530

    added user and tasks collections, and tried examples

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,32 +1,86 @@
 const mongoose = require('mongoose')
 const chalk = require('chalk')
+const validator = require('validator')
+const express = require('express')
 
 const errChalk = chalk.white.bgRed
 const statusChalk = chalk.greenBright.bgBlue
 const log = console.log
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api',{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
     useCreateIndex: true
 })
 
 const User = mongoose.model('User', {
     name: {
-        type: String
+        type: String,
+        required: true,
+        trim: true
     },
     age: {
-        type: Number
+        type: Number,
+        default: 0,
+        validate(value) {
+            if (value < 0) {
+                throw new Error('Age must be valid number')
+            }
+        }
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email is invalid')
+            }
+        }
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 7,
+        trim: true,
+        validate(value) {
+            if (value.toLowerCase().inclues('password')) {
+                throw new Error('Password should not contain "password"')
+            }
+        }
     }
 })
 
-const me = new User({
-    name: 'jax',
-    age: 36
+// const me = new User({
+//     name: 'abhi  ',
+//     email: 'A@A.com  '
+// })
+
+// me.save().then((result) => {
+//     log(statusChalk(result))
+// }).catch((error) => {
+//     log(errChalk(error))
+// })
+
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 })
 
-me.save().then( (result) => {
-    log(statusChalk(result))
-}).catch((error) => {
-    log(errChalk(error))
-})
+// const task = new Task({
+//     description: 'eat lunch',
+//     completed: false
+// }).save().then((result) => {
+//     log(statusChalk(result))
+// }).catch((error) => {
+//     log(errChalk(error))
+// })
